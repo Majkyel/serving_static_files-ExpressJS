f7ba74ddf5f0cbb983152d5a63ce29ec9340fea9
Michał Kuliś <michalkulis1993@gmail.com>
2018-08-11 11:43:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ var app = express();
 
 app.use(express.static('assets'));
 
+
 app.get('/', function(req, res) {
     res.sendFile('./assets/index.html');
+});
+
+app.use(function(req, res, next) {
+    console.log('Hi! I am between request and response!');
+    next();
 });
 
 app.get('/userform', function(req, res) {
@@ -16,13 +22,13 @@ app.get('/userform', function(req, res) {
     res.end(JSON.stringify(response));
 });
 
-app.use(function(req, res) {
-    res.status(404).send('Error. Please check the path!');
-});
-
 var server = app.listen(3000, 'localhost', function() {
     var host = server.address().address;
     var port = server.address().port;
     
     console.log('Application listen on http://'+host+':'+port);
+});
+
+app.use(function(req, res) {
+    res.status(404).send('Error. Please check the path!');
 });
